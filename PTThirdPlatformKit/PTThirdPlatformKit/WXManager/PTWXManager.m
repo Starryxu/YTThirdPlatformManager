ed823efc4bc72846fb06cebf16fa239402d1b3e2
aron <862709539@qq.com>
2017-11-17 10:25:33 +0800

    微信获取APPID修改

--- PTThirdPlatformKit/PTThirdPlatformKit/WXManager/PTWXManager.m
+++ PTThirdPlatformKit/PTThirdPlatformKit/WXManager/PTWXManager.m
@@ -24,7 +24,7 @@ DEF_SINGLETON
 - (void)thirdPlatConfigWithApplication:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // 子类实现
     //向微信注册
-    NSString* appID = [[PTThirdPlatformConfigManager sharedInstance] appKeyWithPlaform:PTThirdPlatformTypeWechat];
+    NSString* appID = [[PTThirdPlatformConfigManager sharedInstance] appIDWithPlaform:PTThirdPlatformTypeWechat];
     [WXApi registerApp:appID];
 }
 
